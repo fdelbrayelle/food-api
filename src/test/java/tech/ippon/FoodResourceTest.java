8a53044dcac8d3f5d47ca51246c7ddb1ec15981b
François Delbrayelle <fdelbrayelle@gmail.com>
2020-02-12 14:24:29 +0100

    feat: food management

--- src/test/java/tech/ippon/FoodResourceTest.java
+++ src/test/java/tech/ippon/FoodResourceTest.java
@@ -4,18 +4,16 @@ import io.quarkus.test.junit.QuarkusTest;
 import org.junit.jupiter.api.Test;
 
 import static io.restassured.RestAssured.given;
-import static org.hamcrest.CoreMatchers.is;
 
 @QuarkusTest
-public class ExampleResourceTest {
+public class FoodResourceTest {
 
     @Test
     public void testHelloEndpoint() {
         given()
-          .when().get("/hello")
+          .when().get("/foods")
           .then()
-             .statusCode(200)
-             .body(is("hello"));
+             .statusCode(200);
     }
 
 }
--- src/test/java/tech/ippon/NativeFoodResourceIT.java
+++ src/test/java/tech/ippon/NativeFoodResourceIT.java
@@ -3,7 +3,7 @@ package tech.ippon;
 import io.quarkus.test.junit.NativeImageTest;
 
 @NativeImageTest
-public class NativeExampleResourceIT extends ExampleResourceTest {
+public class NativeFoodResourceIT extends FoodResourceTest {
 
     // Execute the same tests but in native mode.
 }
